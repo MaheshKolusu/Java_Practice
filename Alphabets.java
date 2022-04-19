38cc7ec4309301e2e9860c1911be552645f76891
MaheshKolusu <103114734+MaheshKolusu@users.noreply.github.com>
2022-04-19 12:07:20 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Alphabets{
+  public static void main(String[] args)
+  {
+    for(char ch='A'; ch<='Z'; ch++){
+      System.out.println(ch);
+      }
+    }
+      
+  }
